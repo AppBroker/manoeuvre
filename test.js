2639c3cc7e2eb95103215aba1bf27abe09af352c
Ryan McLaughlin <ryanmclaughlin@Ryans-MacBook-Pro.local>
2020-09-03 19:04:02 +0100

    Travis fixes

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 import test from 'ava';
-import unicornFun from '.';
+import manoeuvre from './src';
 
 test('main', t => {
 	t.throws(() => {
-		unicornFun(123);
+		manoeuvre(123);
 	}, {
 		instanceOf: TypeError,
 		message: 'Expected a string, got number'
 	});
 
-	t.is(unicornFun('unicorns'), 'unicorns & rainbows');
+	t.is(manoeuvre('unicorns'), 'unicorns & rainbows');
 });
