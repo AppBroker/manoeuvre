b60381ca18ce231e3927388788a13afafea4fc22
Ryan McLaughlin <ryanmclaughlin@Ryans-MacBook-Pro.local>
2020-09-04 12:38:59 +0100

    Moving stuff about

--- src/api.js
+++ src/api.js
@@ -11,16 +11,18 @@ const Api = (client) => {
 }
  */
 // http://customermanager.mybluemix.net/api/userservice/affiliate_add
-Api.prototype.add = (args, done) => {
+Api.prototype.add = (args = {}, done) => {
   const endpoint = 'affiliate_add';
-  args.body = {	'device_type': args.device_type,
-				'ip_address': args.ip_address,
-				'status': args.status,
+  const payload = { ...args };
+  payload.body = {
+    device_type: args.device_type,
+    ip_address: args.ip_address,
+    status: args.status,
+  };
+  if (('config' in args) && ('valid_for' in args.config)) {
+    payload.config = { valid_for: payload.config.valid_for };
   }
-  if(('config' in args) && ('valid_for' in args.config)){
-	  args.config = {'valid_for' : args.config.valid_for};
-  }
-  return this.client.postEndpoint(endpoint, args, done);
+  return this.client.postEndpoint(endpoint, payload, done);
 };
 /*
 {
@@ -32,22 +34,25 @@ Api.prototype.add = (args, done) => {
 }
  */
 // http://customermanager.mybluemix.net/api/userservice/affiliate_update
-Api.prototype.update = (args, done) => {
+Api.prototype.update = (args = {}, done) => {
   const endpoint = 'affiliate_update';
-  args.body = {	'device_type': args.device_type,
-				'ip_address': args.ip_address,
-				'uuid': args.uuid,
-				'status': args.status,
-				'id': args.id};
-  return this.client.postEndpoint(endpoint, args, done);
+  const payload = { ...args };
+  payload.body = {
+    device_type: args.device_type,
+    ip_address: args.ip_address,
+    uuid: args.uuid,
+    status: args.status,
+    id: args.id,
+  };
+  return this.client.postEndpoint(endpoint, payload, done);
 };
 
 // http://customermanager.mybluemix.net/api/userservice/affiliate_stats/affid
 Api.prototype.stats = (args, done) => {
-	const affId = args.affiliate_id
-	const endpoint = 'affiliate_stats/' + affId;
-	// TODO change back to get
-	return this.client.postEndpoint(endpoint, args, done);
-}
+  const affId = args.affiliate_id;
+  const endpoint = `affiliate_stats/${affId}`;
+  // TODO change back to get
+  return this.client.postEndpoint(endpoint, args, done);
+};
 
-module.exports = Api;
+export default Api;
